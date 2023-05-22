8139cf5f663ccc608899a048ecaed7a1750ff3c8
mynameisSergey <116033520+mynameisSergey@users.noreply.github.com>
2023-05-22 22:47:50 +0300

    Добрый день! Заранее благодарен за замечания

--- src/main/java/ru/practicum/shareit/exception/NullParamException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NullParamException extends RuntimeException {
-    public NullParamException(final String message) {
-        super(message);
-    }
-}
