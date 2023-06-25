6bcd7799d64d591e98d596fbfed3975d7586f4e5
mynameisSergey <116033520+mynameisSergey@users.noreply.github.com>
2023-06-25 08:54:22 +0300

    Добрый день! спринт 15 1 версия

--- src/main/java/ru/practicum/shareit/booking/State.java
+++ src/main/java/ru/practicum/shareit/booking/State.java
@@ -1,14 +1,5 @@
 package ru.practicum.shareit.booking;
 
 public enum State {
-    ALL, CURRENT, PAST, FUTURE, WAITING, REJECTED, UNSUPPORTED_STATUS;
-
-    public static State fromValue(String value) {
-        for (State state : values()) {
-            if (state.toString().equals(value)) {
-                return state;
-            }
-        }
-        return UNSUPPORTED_STATUS;
-    }
+    WAITING, REJECTED, ALL, CURRENT, PAST, FUTURE, UNSUPPORTED_STATUS
 }
