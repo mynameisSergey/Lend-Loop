c2443167068ebfecc4a3e3e8426a6cbe7badeb36
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-20 20:41:19 +0300

    docs: добавлена информация о проекте в файл README

--- server/src/test/java/shareit/item/ItemControllerTest.java
+++ server/src/test/java/shareit/item/ItemControllerTest.java
@@ -2,7 +2,6 @@ package shareit.item;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
