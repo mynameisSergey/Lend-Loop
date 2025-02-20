9cb8200cbfe22e23c6bc08cf264cd866462d8416
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-20 22:30:25 +0300

    fix: удалена валидация в контроллерах сервера

--- server/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -9,9 +9,8 @@ import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import java.util.List;
+
 @RequiredArgsConstructor
 @Slf4j
 @RestController
@@ -25,47 +24,47 @@ public class ItemController {
 
     @PostMapping
     public ResponseEntity<ItemDto> create(@RequestHeader(XSHARERUSERID) Long userId,
-                                          @Valid @RequestBody ItemDto itemDto) {
+                                          @RequestBody ItemDto itemDto) {
         log.info("POST запрос на создание новой вещи: {} от пользователя c id: {}", itemDto, userId);
         return ResponseEntity.ok(itemService.create(userId, itemDto));
     }
 
     @PatchMapping("/{itemId}")
     public ResponseEntity<ItemDto> update(@RequestHeader(XSHARERUSERID) Long userId,
-                          @Valid @RequestBody ItemDto itemDto,
-                          @PathVariable("itemId") Long itemId) {
+                                          @RequestBody ItemDto itemDto,
+                                          @PathVariable("itemId") Long itemId) {
         log.info("PATCH запрос на обновление вещи id: {} пользователя c id: {}", itemId, userId);
         return ResponseEntity.ok(itemService.update(userId, itemId, itemDto));
     }
 
     @GetMapping("/{itemId}")
     public ResponseEntity<ItemDto> get(@RequestHeader(XSHARERUSERID) Long userId,
-                       @PathVariable Long itemId) {
+                                       @PathVariable Long itemId) {
         log.info("GET запрос на получение вещи c id: {}", itemId);
         return ResponseEntity.ok(itemService.getItemById(userId, itemId));
     }
 
     @GetMapping
     public ResponseEntity<List<ItemDto>> getAll(@RequestHeader(XSHARERUSERID) Long userId,
-                                @RequestParam(name = "from", defaultValue = "0") @Min(0) Integer from,
-                                @RequestParam(name = "size", defaultValue = "10") @Min(1) Integer size) {
+                                                @RequestParam(name = "from", defaultValue = "0") Integer from,
+                                                @RequestParam(name = "size", defaultValue = "10") Integer size) {
         log.info("GET запрос на получение всех вещей пользователя c id: {}", userId);
         return ResponseEntity.ok(itemService.getAll(userId, from, size));
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<ItemDto>> searchItems(@RequestHeader(XSHARERUSERID) Long userId,
-                                     @RequestParam(name = "text") String text,
-                                     @RequestParam(name = "from", defaultValue = "0") @Min(0) Integer from,
-                                     @RequestParam(name = "size", defaultValue = "10") @Min(1) Integer size) {
+                                                     @RequestParam(name = "text") String text,
+                                                     @RequestParam(name = "from", defaultValue = "0") Integer from,
+                                                     @RequestParam(name = "size", defaultValue = "10") Integer size) {
         log.info("GET запрос на поиск всех вещей c текстом: {}", text);
         return ResponseEntity.ok(itemService.search(userId, text, from, size));
     }
 
     @PostMapping("/{itemId}/comment")
     public ResponseEntity<CommentDto> createComment(@RequestHeader(XSHARERUSERID) Long userId,
-                                    @Validated @RequestBody CommentDto commentDto,
-                                    @PathVariable Long itemId) {
+                                                    @Validated @RequestBody CommentDto commentDto,
+                                                    @PathVariable Long itemId) {
         return ResponseEntity.ok(itemService.createComment(userId, commentDto, itemId));
     }
 }
