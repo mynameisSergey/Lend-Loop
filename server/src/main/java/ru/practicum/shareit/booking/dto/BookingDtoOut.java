22f236bdee14306e89074b671466f7100b90bb8b
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-28 19:00:42 +0300

    fix: исправить аннотации BookingItemDto

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoOut.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoOut.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,15 +22,18 @@ import java.time.LocalDateTime;
 public class BookingDtoOut {
     private Long id;
     private Item item;
+
     @NotNull
     @FutureOrPresent
-    @DateTimeFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime start;
+
     @NotNull
     @Future
-    @DateTimeFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime end;
     private User booker;
     private BookingStatus status;
+
 }
 
--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingItemDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingItemDto.java
@@ -8,4 +8,5 @@ import lombok.Data;
 public class BookingItemDto {
     private Long id;
     private Long bookerId;
+
 }
